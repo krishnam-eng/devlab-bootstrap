6feb04a0bd68e900a10ca797e1146fa7ff9f5089
Balamurugan Krishnamoorthy <krishnam.balamurugan.eng@gmail.com>
2021-04-11 01:26:15 +0530

    launch layout setup

--- tmux/sessions/launch-linuxenv-proj.sh
+++ tmux/sessions/launch-linuxenv-proj.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/bash
 
-tmux has-session -t proj-linuxenv
-if [ $?!=0 ]
-then
-	tmux new-session -s proj-linuxenv -n editor -d
+session="proj-linux";
 
-	tmux send-keys -t proj-linuxenv 'cd ~/ghrepo/oh-my-linux-env' C-m
-	tmux send-keys -t proj-linuxenv 'nano tmux/.tmux.conf' C-m
+tmux has-session -t $session
 
-	tmux split-window -v -p 20 -t proj-linuxenv
+if [$?!=0]; then
+	tmux new-session -s $session -n editor -d
 
-	tmux send-keys -t proj-linuxenv:1.2 'cd ~/ghrepo/oh-my-linux-env' C-m
-	tmux send-keys -t proj-linuxenv:1.2 'git status' C-m
+	tmux send-keys -t $session 'cd ~/ghrepo/oh-my-linux-env' C-m
+	tmux send-keys -t $session 'nano tmux/.tmux.conf' C-m
 
-	tmux split-window -h -p 20 -t proj-linuxenv
+	tmux split-window -v -p 20 -t $session
 
-	tmux send-keys -t proj-linuxenv:1.3 'top' C-m
+	tmux send-keys -t $session:1.2 'cd ~/ghrepo/oh-my-linux-env' C-m
+	tmux send-keys -t $session:1.2 'git status' C-m
 
-	tmux new-window -n reference -t proj-linuxenv
+	tmux split-window -h -p 20 -t $session
 
-	tmux split-window -h -t proj-linuxenv:2
-	tmux send-keys -t proj-linuxenv:2.1 'nano ~/ghrepo/oh-my-linux-env/zsh/.zshrc' C-m
-	tmux send-keys -t proj-linuxenv:2.1 'nano ~/ghrepo/oh-my-linux-env/alias/.alias' C-m
-	tmux select-pane -t proj-linuxenv:2.1
+	tmux send-keys -t $session:1.3 'top' C-m
 
-	tmux select-window -t proj-linuxenv:1
+	tmux new-window -n config -t $session
+	tmux send-keys -t $session:2.1 'nano ~/ghrepo/oh-my-linux-env/zsh/.zshrc' 
+
+	tmux split-window -h -t $session:2
+	tmux send-keys -t $session:2.2 'nano ~/ghrepo/oh-my-linux-env/alias/.alias' C-m
+	tmux select-pane -t 1
+
+	tmux new-window -n manual -t $session
+	tmux send-keys -t $session:3.1 'man tmux' C-m
+	tmux split-window -h -t $session:3
+	tmux select-pane -t 1
+
+	tmux select-window -t $session:1
+	tmux select-pane -t 1
 fi
 
-tmux attach -t proj-linuxenv
+tmux attach-session -t $session

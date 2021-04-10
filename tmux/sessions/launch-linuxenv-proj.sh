9ffbaf2b67a83f0c5c392c989d4cb35f00616438
Balamurugan Krishnamoorthy <krishnam.balamurugan.eng@gmail.com>
2021-04-11 01:37:18 +0530

    launch script repo variable

--- tmux/sessions/launch-linuxenv-proj.sh
+++ tmux/sessions/launch-linuxenv-proj.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/bash
 
-session="proj-linux";
+session="proj-linux"
+repo_path="~/ghrepo/oh-my-linux-env"
 
-tmux has-session -t $session
+tmux has-session -t $session 2>/dev/null
 
-if [$?!=0]; then
+if [ $? != 0 ]
+then
 	tmux new-session -s $session -n editor -d
 
-	tmux send-keys -t $session 'cd ~/ghrepo/oh-my-linux-env' C-m
-	tmux send-keys -t $session 'nano tmux/.tmux.conf' C-m
+	tmux send-keys -t $session "cd $repo_path" C-m
+	tmux send-keys -t $session "nano $repo_path/tmux/.tmux.conf" C-m
 
 	tmux split-window -v -p 20 -t $session
 
-	tmux send-keys -t $session:1.2 'cd ~/ghrepo/oh-my-linux-env' C-m
-	tmux send-keys -t $session:1.2 'git status' C-m
+	tmux send-keys -t $session:1.2 "cd $repo_path" C-m
+	tmux send-keys -t $session:1.2 "git status" C-m
 
 	tmux split-window -h -p 20 -t $session
 
-	tmux send-keys -t $session:1.3 'top' C-m
+	tmux send-keys -t $session:1.3 "top" C-m
 
 	tmux new-window -n config -t $session
-	tmux send-keys -t $session:2.1 'nano ~/ghrepo/oh-my-linux-env/zsh/.zshrc' 
+	tmux send-keys -t $session:2.1 "nano $repo_path/zsh/.zshrc" 
 
 	tmux split-window -h -t $session:2
-	tmux send-keys -t $session:2.2 'nano ~/ghrepo/oh-my-linux-env/alias/.alias' C-m
+	tmux send-keys -t $session:2.2 "nano $repo_path/alias/.alias" C-m
 	tmux select-pane -t 1
 
 	tmux new-window -n manual -t $session
-	tmux send-keys -t $session:3.1 'man tmux' C-m
+	tmux send-keys -t $session:3.1 "man tmux" C-m
 	tmux split-window -h -t $session:3
 	tmux select-pane -t 1
 

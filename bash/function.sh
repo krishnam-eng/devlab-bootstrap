b48be3ed6b7c925236a3d8033999d9d10920c70d
Balamurugan Krishnamoorthy <krishnam.balamurugan.eng@gmail.com>
2021-04-11 14:48:15 +0530

    setting work session layout for bash practice

---
+++ bash/function.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+mk_bkp_dir_for_file()
+{
+	abs_src_filepath=$1
+
+	dir_name=dirname $abs_src_filepath
+	bkp_dir="~/bkp/$dirname"
+	mkdir -p $bkp_dir
+	return $bkp_dir
+}
+
+
+take_bkp()
+{
+	abs_src_filepath=$1
+	bkp_dir = mk_bkp_dir_for_file($abs_src_filepath)
+	filename = filename $abs_src_filepath
+	target_filename=$filename_$(date +%FT%T)
+	echo $target_filename
+	echo $bkp_dir
+	cp $abs_src_filepath $bkp_dir/$target_filename
+}
---
+++ bash/up_n_running/prog_with_bash.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# => This is a comment
+# Use env to find bash instead of hardcoding bash path
+
+
+echo "hello"
+
+echo "there"
+
+
+
+
+
+

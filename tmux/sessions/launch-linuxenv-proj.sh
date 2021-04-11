9158644de64a4b04bf15d5527bcfee449de9f92e
Balamurugan Krishnamoorthy (Bala) <82016952+krishnam-eng@users.noreply.github.com>
2021-04-11 07:34:29 +0530

    window for ranger

--- tmux/sessions/launch-linuxenv-proj.sh
+++ tmux/sessions/launch-linuxenv-proj.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 session="proj-linux"
-repo_path="~/ghrepo/oh-my-linux-env"
+repo_path="~/ghrepo/ohmy-linux"
 
 tmux has-session -t $session 2>/dev/null
 
@@ -26,15 +26,18 @@ then
 
 	tmux split-window -h -t $session:2
 	tmux send-keys -t $session:2.2 "nano $repo_path/alias/.alias" C-m
-	tmux select-pane -t 1
-
-	tmux new-window -n manual -t $session
-	tmux send-keys -t $session:3.1 "man tmux" C-m
-	tmux split-window -h -t $session:3
-	tmux select-pane -t 1
-
-	tmux select-window -t $session:1
-	tmux select-pane -t 1
+        tmux select-pane -t $session:2.1                                                        
+                                                                                            
+       tmux new-window -n manual -t $session                                                   
+       tmux send-keys -t $session:3.1 "man tmux" C-m                                           
+       tmux select-pane -t $session:3.1                                                        
+       tmux split-window -h -t $session:3                                                      
+       
+       tmux new-window -n fbrowser -t $session                                                 
+       tmux send-keys -t $session:4.1 "ranger 2>/dev/null" C-m                                             
+                                                                                             
+       tmux select-window -t $session:1                                                        
+       tmux select-pane -t $session:1.1    
 fi
 
 tmux attach-session -t $session

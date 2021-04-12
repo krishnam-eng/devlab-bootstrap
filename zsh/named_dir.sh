9d69e673dfd6b480f5a224d8c443b74b9c1a662a
Balamurugan Krishnamoorthy <krishnam.balamurugan.eng@gmail.com>
2021-04-12 12:01:13 +0530

    zdotfile: using it to control the conf file org

--- zsh/named_dir.sh
+++ zsh/named_dir.sh
@@ -4,11 +4,22 @@
 # What: Creating Named Dir by adding ~shortcutname entry in "fil/dir names hash table."
 #
 # Why: named dir can be called like ~shortcutname  similar to how ~ refers to home dir
-# default: hash -d =/home/krishnam # ~#
+#
+# named directory or user are refered with ~ prefix 
 ###################
 
-hash -d gh=~/github # ~gh
+# quick access to config files
+hash -d zsh=~/.myzsh      # ~zsh
+hash -d nano=~/.mynano    # ~nano
+hash -d tmux=~/.mytmux    # ~tmux
+hash -d bash=~/.mybash    # ~bash
+
+
+# quick access to repos
+hash -d gh=~/github               # ~gh
 hash -d lrepo=~/github/ohmy-linux # ~lrepo
+
+
 
 
 

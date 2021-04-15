065a762ea56f3bf1e6a3dd0a9c892bf4c3fea530
Balamurugan Krishnamoorthy (Bala) <82016952+krishnam-eng@users.noreply.github.com>
2021-04-15 10:20:22 +0530

    Update install_tmux_and_ext.sh

--- tmux/install_tmux_and_ext.sh
+++ tmux/install_tmux_and_ext.sh
@@ -1,3 +1,5 @@
+# I am currently using both ubuntu and centos. So, you will find commands for both here.
+
 # Install tmux on Ubuntu / Debian
 sudo apt-get update
 sudo apt-get install tmux
@@ -5,3 +7,4 @@ sudo apt-get install tmux
 # Install tmux on Fedora / CentOS
 sudo yum -y install tmux
 
+#todo: add cmds of used plugins setup

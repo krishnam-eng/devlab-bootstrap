73bd15f8f647e3e77a33ad6386ede2b3234584f1
Balamurugan Krishnamoorthy <krishnam.balamurugan.eng@gmail.com>
2021-04-16 12:07:51 +0530

    install dev tools script

--- apt/install_dev_tools.ubuntu21.sh
+++ apt/install_dev_tools.ubuntu21.sh
@@ -2,3 +2,33 @@
 # take a log of manually installed packages and its dependent packages
 apt list --manual-installed >| ~/github/ohmy-linux/apt/useful_manually_installed-dev_apt-$(date +%Y%m).list 2>/dev/null
 
+# take install cmds from histroy 
+history  | grep -e "sudo apt-get install " | cut -c 8-  >> ~/github/ohmy-linux/apt/install_dev_tools.ubuntu21.sh 
+history  | grep -e "sudo apt install " | cut -c 8-  >> ~/github/ohmy-linux/apt/install_dev_tools.ubuntu21.sh
+
+sudo apt-get install chrome
+
+sudo apt-get install zsh
+
+sudo apt-get install unzip
+sudo apt-get install tree
+
+#  used in nano
+sudo apt-get install spell
+sudo apt-get install vim
+
+sudo apt-get install git
+
+sudo apt-get install gcc
+sudo apt-get install perl
+sudo apt-get install default-jdk
+
+sudo apt-get install apache2
+sudo apt-get install tomcat9
+sudo apt-get install tomcat9-admin tomcat9-docs tomcat9-examples
+
+sudo apt-get install libxml2-utils
+
+
+
+

648ea00d9c43460dade75865b17f5e3ab3848b54
Balamurugan Krishnamoorthy <krishnam.balamurugan.eng@gmail.com>
2021-04-18 11:58:12 +0530

    -msetup python dev env

--- apt/install_dev_tools.ubuntu21.sh
+++ apt/install_dev_tools.ubuntu21.sh
@@ -29,6 +29,28 @@ sudo apt-get install tomcat9-admin tomcat9-docs tomcat9-examples
 
 sudo apt-get install libxml2-utils
 
+######## python
 
+sudo apt install python3.9
+
+sudo update-alternatives --config python3
+
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+sudo apt install python3.9
+
+
+sudo ln -s /usr/bin/python3 /usr/bin/python
+sudo ln -s /usr/bin/python3.9 /usr/bin/python3
+
+ls -lh /usr/bin/python*
+#lrwxrwxrwx 1 root root   16 Apr 18 11:21 /usr/bin/python -> /usr/bin/python3*
+#lrwxrwxrwx 1 root root   18 Apr 18 11:22 /usr/bin/python3 -> /usr/bin/python3.9*
+#-rwxr-xr-x 1 root root 5.1M Jan 27 21:12 /usr/bin/python3.8*
+#-rwxr-xr-x 1 root root 5.3M Oct 19 15:21 /usr/bin/python3.9*
+
+##### IDE
+firefox https://code.visualstudio.com/Download
+sudo dpkg -i ~/dwnlds/code_1.55.2-16183│07277_amd64.deb
 
 
--- apt/install_dev_tools.win10.sh
+++ apt/install_dev_tools.win10.sh
@@ -1,4 +1,12 @@
-
+######## SSH
 # PuTTY     - SSH and telnet client
 # FileZilla - FTP over SSL/TLS
+
+
+######## python
+# VS Code
+# sudo apt install python3.9
+# sudo update-alternatives --config python3
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
 

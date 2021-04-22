fe42259a2af48fe5f5b4cc49675f056d97229f0b
Balamurugan Krishnamoorthy (Bala) <82016952+krishnam-eng@users.noreply.github.com>
2021-04-22 15:04:03 +0530

    Update build_filesystem_hierarchy.sh

--- boot/build_filesystem_hierarchy.sh
+++ boot/build_filesystem_hierarchy.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
 
-# checkout github repos
+###### checkout github repos
 mkdir -p ~/github
 
+# Setup SSH pub key for secure interaction with github
+# ssh-keygen -t ed25519 -C "krishnam.balamurugan.eng@gmail.com" 
+# cat gh_id_ed25519.pub 
+
+# copy public key and create new key in github with this 
+# github->"Setting" -> "SSH and GPG keys". Now we are ready for git clone  
+
+
 if [[ ! -d ~/github/ohmy-linux ]]; then
-    echo "cloning ohmy-linux repo"
-    git clone https://github.com/krishnam-eng/ohmy-linux ~/github/ohmy-linux
+    echo "cloning ohmy-linux repo..."
+    git clone git@github.com:krishnam-eng/ohmy-linux  ~/github/ohmy-linux
 fi
 
 if [[ ! -d ~/github/practice-python ]]; then
-    echo "cloning practice-python repo"
-    git clone https://github.com/krishnam-eng/practice-python ~/github/practice-python
-    git remote set-url origin git@github.com:krishnam-eng/practice-python.git
+    echo "cloning practice-python repo..."
+    git clone git@github.com:krishnam-eng/practice-python.git ~/github/practice-python
+fi
+
+if [[ ! -d ~/github/practice-java-performance-tuning ]]; then
+    echo "cloning practice-python repo..."
+    git clone git@github.com:krishnam-eng/practice-java-performance-tuning.git ~/github/practice-java-performance-tuning
 fi

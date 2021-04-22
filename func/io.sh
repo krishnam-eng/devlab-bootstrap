d3f52145dd3d58e2f7c7d1dd6d1ce543be6d4d03
Balamurugan Krishnamoorthy <krishnam.balamurugan.eng@gmail.com>
2021-04-22 11:52:36 +0530

    bash func

--- func/io.sh
+++ func/io.sh
@@ -19,3 +19,29 @@ function choose {
         * ) printf "%b" "Unexpected answer '$answer'!";;
     esac
 }
+
+# Let the user make a choice about something and return a standardized
+# answer. How the default is handled and what happens next is up to
+# the if/then after the choice in main.
+# Called like: choice <prompt>
+# e.g. choice "Do you want to play a game?"
+# Returns: global variable CHOICE
+
+function choice {
+    CHOICE=''
+    local prompt="$*"
+    local answer
+    read -p "$prompt" answer
+    case "$answer" in
+        [yY1] ) CHOICE='y';;
+        [nN0] ) CHOICE='n';;
+        * ) CHOICE="$answer";;
+    esac
+}
+
+# Usage Example:
+# choice "Do you want to look at the error logfile? [Y/n]: "
+# if [ "$CHOICE" != "n" ]; then
+# less error.log
+# fi
+

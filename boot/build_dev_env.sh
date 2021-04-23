8e83ca3b1e70c96e20351762acf9aa4cbbf85c2d
Balamurugan Krishnamoorthy (Bala) <82016952+krishnam-eng@users.noreply.github.com>
2021-04-23 09:17:34 +0530

    install zsh

--- boot/build_dev_env.sh
+++ boot/build_dev_env.sh
@@ -9,9 +9,12 @@
 #####
 #  Install List
 #####
-sudo apt install xclip
 sudo apt install git
-sudo apt  install tmux
+sudo apt install zsh
+sudo apt install tmux
+sudo apt install nano
+
+sudo apt install xclip
 
 ######
 # checkout dev env github repo
@@ -31,6 +34,12 @@ if [[ ! -d ~/github/ohmy-linux ]]; then
     
     # if it was cloned from http, use below to change to ssh
     # git remote set-url origin git@github.com:krishnam-eng/practice-python.git
+fi
+
+# TMUX plugin manager
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+  mkdir -p  ~/.tmux/plugins/
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 #####

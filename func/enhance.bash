27b12e885f26a3393f3ac3b7788a1b14956b1a00
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-04-23 08:53:37 +0530

    bash func

--- func/enhance.bash
+++ func/enhance.bash
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+################
+#description  : bash functions to override or provide addtional details based on basic commands 
+#bash_version : 5.1.4(1)-release
+#author	      : krishnam
+################
 
-source ~wsrepo/func/colorama.sh
+source ~/.myfunc/colorama.bash
 
 # P: Showing Where You Are - show working directory 
 
-fn swd(){
-	echo $cs_bblue"Logical Path:$cs_reset $(command pwd -L )" # print working dir logical path
+function swd {
+  echo $cs_bblue"Logical Path:$cs_reset $(command pwd -L )" # print working dir logical path
 	echo $cs_bgreen"Physical Path:$cs_reset $(command pwd -P )" # physical location - useful for syslink
 }
 
@@ -17,32 +22,28 @@ fn swd(){
 #
 # Option: -s search in manpages
 
-fn fcmd(){
-    echo $cs_bcyan"[whence]:$cs_byellow $(whence $1)"$cs_reset
-    echo $cs_bcyan"[type]  :$cs_byellow $(type $1)"$cs_reset
-    echo $cs_bcyan"[which] :$cs_byellow $(which $1)"$cs_reset
-
-    while getopts "s" opt
-    do
-        case $opt in
-            s) echo $cs_bcyan"[apropos]:$cs_reset $(apropos $1)";;
-        esac
-    done
+function fcmd {
+  echo $cs_bcyan"[whence]:$cs_byellow $(whence $1)"$cs_reset
+  echo $cs_bcyan"[type]  :$cs_byellow $(type $1)"$cs_reset
+  echo $cs_bcyan"[which] :$cs_byellow $(which $1)"$cs_reset
+  while getopts "s" opt
+  do
+    case $opt in
+      s) echo $cs_bcyan"[apropos]:$cs_reset $(apropos $1)";;
+    esac
+  done
 }
 
 # file info
-fn finfo(){
-    echo $cs_bcyan"[ls]$cs_reset $(ls -l $1)"
-    echo $cs_byellow"[file]$cs_reset $(file $1)"
+function finfo {
+  echo $cs_bcyan"[ls]$cs_reset $(ls -l $1)"
+  echo $cs_byellow"[file]$cs_reset $(file $1)"
 	echo $cs_bgreen"[stat]$cs_reset $(stat $1)"
 }
 
-# show only dot files
-alias ld="ls -d"
-
 # ubuntu update
-fn upkg(){
-    echo $cs_bcyan"UPDATE....."$cs_reset
+function upkg {
+  echo $cs_bcyan"UPDATE....."$cs_reset
 	sudo apt-get update
 
 	echo $cs_bgreen"UPGRADE....."$cs_reset

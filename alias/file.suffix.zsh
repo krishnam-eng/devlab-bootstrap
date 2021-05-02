ebb53eb9734370135ba076b067f0500a61e9b977
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-05-02 07:19:25 +0530

    zsh suffix & global alias setup

---
+++ alias/file.suffix.zsh
@@ -0,0 +1,21 @@
+#########==========---------
+# Suffix Aliases
+#
+#   Suffix Aliases are defined using the -s flag. With suffix aliases, you can launch files with a specific extension (or suffix) in your favorite tool.
+#########==========---------
+
+# markdown files
+alias -s md=code
+
+# Python files
+alias -s py=code
+
+# shell
+alias -s {sh,bash,zsh}=nano
+
+# run commands file
+alias -s {zshrc,nanorc,bashrc}=nano
+
+# config files
+alias -s config=nano
+

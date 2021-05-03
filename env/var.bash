6ae2656b542b4e777f4bd60c67a4012bd07031b3
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-05-03 20:22:07 +0530

    pomo stat

---
+++ env/var.bash
@@ -0,0 +1,25 @@
+# recipe: using params in shell
+#   $1,$2...${10}
+
+# recipe: looping over arg passed
+#   $* , $@ - quoted
+
+# recipe: counting args
+#   $#
+
+#  recipe: getting default if no arg => ${:-}
+#   ${1:-defaultvalue } or ${:-"$(someothervar)"}
+
+#  recipe: setting default if no arg => ${:=}
+#   ${HOME:=defaultvalue }
+
+
+# declare with case type
+#   declare -u UP
+#   declare -l lower
+#   declare -c Ca - upper first
+
+
+
+
+

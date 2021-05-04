ab4ca1c18c3457b737996c5acf64d0579bd19956
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-05-04 20:18:32 +0530

    venv wrapper config

--- alias/launch.bash
+++ alias/launch.bash
@@ -5,6 +5,38 @@
 # launch visual studio in bg
 alias vscode='code &'
 
+##### python venv ########
+# create virtual env with py3
+alias venv="virtualenv --python python3"
+
+# activate venv
+avenv (){
+  source ${1}/bin/activate
+}
+
+# snapshot venv requirements
+ssvenv (){
+  mkdir -p venv
+  pip3 freeze > venv/requirements.venv
+}
+
+# recreate venv from snapshot
+alias rvenv="pip3 install -r venv/requirements.venv"
+
+# load virtual env from snapshot
+
+# To use venv wrapper extention
+alias mkvenv="mkvirtualenv"
+
+# Activates the virtual environment or switches from the current environment to the specified one
+alias svenv="workon" #switch or start workspace
+
+# Deactivates (kill) the virtual environment.
+alias kvenv="deactivate"
+
+
+
+
 # use neo vim
 alias vim="nvim"
 alias vi="nvim"

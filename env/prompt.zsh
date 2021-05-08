62090ea093db781de6396a80101c3fe6e82af9bb
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-05-08 12:28:24 +0530

    venv in right prompt

--- env/prompt.zsh
+++ env/prompt.zsh
@@ -2,3 +2,11 @@
 export PROMPT='%F{154}%n%f@%F{011}%m%F{010}%#%f '
 
 export RPROMPT=' %F{010}[%F{011}%~ %F{154}%*%F{010}]'
+
+if [ -v VIRTUAL_ENV ]
+then
+  echo "env is set"
+  venv_name=$(basename $VIRTUAL_ENV)
+  echo "${venv_name}"
+  export RPROMPT="%F{010}[%F{130}>${venv_name}< %F{011}%~ %F{154}%*%F{010}]"
+fi

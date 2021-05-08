1d015de473bc8812291167c3dcf2c40f65c27451
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-05-08 21:43:25 +0530

    todo note

--- env/prompt.zsh
+++ env/prompt.zsh
@@ -3,6 +3,7 @@ export PROMPT='%F{154}%n%f@%F{011}%m%F{010}%#%f '
 
 export RPROMPT=' %F{010}[%F{011}%~ %F{154}%*%F{010}]'
 
+# gotcha: this file should be sourced once venv is activated
 if [ -v VIRTUAL_ENV ]
 then
   venv_name=$(basename $VIRTUAL_ENV)

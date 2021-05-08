19a415dac3ed06dd5931fcb66182e0263e837bb2
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-05-08 11:23:05 +0530

    alias for py ven & automate project dir nav

--- alias/named_dir.zsh
+++ alias/named_dir.zsh
@@ -13,12 +13,13 @@ hash -d zsh=~/.myzsh      # ~zsh
 hash -d bash=~/.mybash    # ~bash
 hash -d tmux=~/.mytmux    # ~tmux
 hash -d nano=~/.mynano    # ~nano
+hash -d venv=~/.myvenv    # ~venv
 
-# quick access to repos
+# quick access to Repos
 hash -d gh=~/github                     # ~gh
 hash -d lrp=~/github/ohmy-linux         # workspace repo ~lrp
 hash -d alias=~/github/ohmy-linux/alias # ~alias
 hash -d env=~/github/ohmy-linux/env     # ~env
 
-hash -d ltrp=~/github/load-testing      # load testing ~lrp
-hash -d pyrp=~/github/practice-python   # python repo ~pyrp
+hash -d hltrp=~/github/howdoi-loadtest   # load testing ~hltrp
+hash -d pprp=~/github/practice-python   # python repo ~pprp

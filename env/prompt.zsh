74e1f9dd73a5473585493b43226c9f455443311f
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-05-09 08:30:38 +0530

    enhance cd will notice git project dir and quto enable venv

--- env/prompt.zsh
+++ env/prompt.zsh
@@ -2,10 +2,3 @@
 export PROMPT='%F{154}%n%f@%F{011}%m%F{010}%#%f '
 
 export RPROMPT=' %F{010}[%F{011}%~ %F{154}%*%F{010}]'
-
-# gotcha: this file should be sourced once venv is activated
-if [ -v VIRTUAL_ENV ]
-then
-  venv_name=$(basename $VIRTUAL_ENV)
-  export RPROMPT="%F{010}[%F{130}>${venv_name}< %F{011}%~ %F{154}%*%F{010}]"
-fi
--- func/venv.bash
+++ func/venv.bash
@@ -1,24 +1,26 @@
 # Automatically activate Git projects' virtual environments based on the
 # directory name of the project. Virtual environment name can be overridden
 # by placing a venv/name.venv file in the project root with a virtualenv name in it
-# todo: fix the error
-wipcd (){
-    builtin cd "$@"
-    echo "${CS_yellow}$OLDPWD${CS_reset} --> ${CS_bcyan}$PWD${CS_reset}"
 
+gitvenv (){
     # Check that this is a Git repo
     gitdir_rpath=$(git rev-parse --git-dir 2> /dev/null)
     if [ $? -eq 0 ]; then
+
         # Find the repo root and check for virtualenv name override
-        GIT_DIR=$(\cd $gitdir_rpath; pwd)
-        GIT_PROJ_ROOT=`dirname "$GIT_DIR"`
+        export GIT_DIR=$(\cd $gitdir_rpath; pwd)
+        export GIT_PROJ_ROOT=`dirname "$GIT_DIR"`
+
         VENV_NAME=`basename "$GIT_PROJ_ROOT"`
+        echo "${LOG_TS} Identified Git Project: ${CS_bcyan}$VENV_NAME${CS_reset}"
         if [ -f "$GIT_PROJ_ROOT/venv/name.venv" ]; then
             VENV_NAME=$(cat "$GIT_PROJ_ROOT/venv/name.venv")
         fi
+        echo "${LOG_TS} Identified Virtual Environment: ${CS_bcyan}$VENV_NAME${CS_reset}"
         # Activate the environment only if it is not already active
         if [ "$VIRTUAL_ENV" != "$WORKON_HOME/$VENV_NAME" ]; then
             if [ -e "$WORKON_HOME/$VENV_NAME/bin/activate" ]; then
+                echo "${LOG_TS} Activating virtualenv ${VENV_NAME} ..."
                 workon "$VENV_NAME" && export CD_VIRTUAL_ENV="$VENV_NAME"
             fi
         fi
@@ -28,4 +30,3 @@ wipcd (){
         deactivate && unset CD_VIRTUAL_ENV
     fi
 }
-

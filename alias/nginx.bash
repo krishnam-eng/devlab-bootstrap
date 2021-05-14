4f176e3ad3d3240d051d9b0585e5fd93476ad6d0
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-05-14 13:56:00 +0530

    nginx: drop-in config for static content server; alias refactor

--- alias/nginx.bash
+++ alias/nginx.bash
@@ -4,73 +4,79 @@
 # nginx
 ########
 
-# nginx service
-alias ngst="sudo systemctl start nginx"
-alias ngsp="sudo systemctl stop nginx"
-alias ngrl="sudo systemctl reload nginx"
-alias ngia="sudo systemctl is-active nginx" # start & stop comd do not produce any stdout so use this to get the status 
-alias ngss="sudo systemctl status nginx --no-pager" # systemctl uses pager by default, so you need to use q to end status; instead use no-pager switch to get around this
-
-alias ngtcf="sudo nginx -t" # test config and show status
-alias ngtd="sudo nginx -T | less" # test and dump the config
-alias nghp="sudo nginx -h" # help
-
-alias ngps="ps -ef | grep nginx"
-alias ngusr="ps -ef | awk '/nginx.*worker.*process$/{print \$1}'"
-alias ngport="netstat -plan | grep nginx"
+# list all ng alias
+alias ngalias="alias | awk '/^ng/{print}' | lolcat"
 
 # Files & Dirs
-alias ngvelog="less /var/log/nginx/error.log"  # view error log
-alias ngvalog="less /var/log/nginx/access.log" # view access log
+#-------------
 
-alias ngmelog="tail -f /var/log/nginx/error.log"  # monitor error log
-alias ngmalog="tail -f /var/log/nginx/access.log" # monitor access log
-
-
-alias ngvse="la /etc/nginx/sites-enabled"      # view sites enabled
-alias ngvcf="cat /etc/nginx/nginx.conf"      # view conf
-
-alias ngecf="sudo $EDITOR /etc/nginx/nginx.conf"     # edit conf
-
-alias ngcdetc="cd /etc/nginx"                # p\yetsee enginex\
-alias ngcdlog="cd /var/log/nginx"            # web service log
-alias ngcdsa="cd /etc/nginx/sites-available" # server conf of sites
+alias ngcdcr="cd /etc/nginx"                 # configuration-root
+alias ngcdlog="cd /var/log/nginx"            # default log location
+alias ngcdsa="cd /etc/nginx/sites-available" # server conf of available sites
 alias ngcdse="cd /etc/nginx/sites-enabled"   # server conf of enabled sites
+alias ngcdcd="cd /etc/nginx/conf.d"          # default http-site config-dropins dir
 alias ngcdrt="cd /var/www"                   # default root folder for web server
+
+alias ngvel="less /var/log/nginx/error.log"    # view error-log
+alias ngval="less /var/log/nginx/access.log"   # view access-log
+alias ngvse="la /etc/nginx/sites-enabled"      # view sites enabled
+alias ngvsa="la /etc/nginx/sites-available"    # view sites available
+alias ngvcd="la /etc/nginx/conf.d"             # view default http-site configs
+alias ngvcf="cat /etc/nginx/nginx.conf"        # view conf entry point
+
+alias ngmel="tail -f /var/log/nginx/error.log"   # monitor error-log using tail flow
+alias ngmal="tail -f /var/log/nginx/access.log"  # monitor access-log using tail flow
+
+alias ngecf="sudo $EDITOR /etc/nginx/nginx.conf" # edit conf
+
+# nginx service commands
+#-----------------------
+
+# let's make start and stop cmd to be more verbose
+ngst(){
+  sudo systemctl start nginx;
+  sudo systemctl status nginx --no-pager -l;
+}
+
+ngsp(){
+  sudo systemctl stop nginx; # or nginx -s stop
+  sudo systemctl status nginx --no-pager -l;
+}
+
+# reload after testing config - makes it easy to stop issues
+ngrl(){
+  sudo nginx -t;
+  sudo systemctl reload nginx;
+  sudo systemctl status nginx --no-pager -l;
+}
+
+# stop after it finishes processing inflight requests
+alias ngqt="sudo nginx -s quit"
+
+# start & stop cmd do not produce any stdout so use this to get the status
+alias ngia="sudo systemctl is-active nginx"
+
+# test config and show status
+alias ngtcf="sudo nginx -t"
+
+# test-dump config
+alias ngtdcf="sudo nginx -T | less"
+
+# help
+alias nghp="sudo nginx -h"
+
+# systemctl uses pager by default, so you need to use q to end status; instead use no-pager switch to get around this
+alias ngss="sudo systemctl status nginx --no-pager -l"
+
+# show process, usr, port
+alias ngsps="ps -ef | grep nginx"
+alias ngsusr="ps -ef | awk '/nginx.*worker.*process$/{print \$1}'"
+alias ngspt="netstat -plan | grep nginx"
 
 # Back Up: to version control
 ngbkpcf(){
   \cp /etc/nginx/nginx.conf ~/github/ohmy-linux/nginx/nginx.conf
 }
 
-# list open files - ls port listen
-alias lspln="sudo lsof -i | grep LISTEN"
-alias ngv
-
-
-#########
-# apache2
-#########
-# status
-alias ast="sudo systemctl status apache2" # ubuntu
-#alias ast="sudo systemctl status httpd" # centos
-
-# config
-alias aconf="nano /etc/apache2/apache2.conf" # ubuntu
-# alias aconf="nano /etc/httpd/conf/httpd.conf" # centos
-
-##
-# todos:
-#   [] create nameddir for these cd paths
-##
-
-###################################################################
-#   alias pattern:
-#         who{1,2} 'ng'
-#       + what{1,3} 'e -edit, v -view, cd, sp - stop'
-#       + where{2,3} 'log, cf - config'
-#
-#       abbrev:
-#         =^(.) + (.)$ if the word is ((last || middle) && length >4)
-#        +=^(.) for word in compound-word
-###################################################################
+# list listen prots using of:open-files
+alias lslp="sudo lsof -i | grep LISTEN"

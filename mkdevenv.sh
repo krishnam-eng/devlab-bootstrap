51f3a248cde93475bd9e04de916319150ad57b6d
Balamurugan Krishnamoorthy (Bala) <82016952+krishnam-eng@users.noreply.github.com>
2021-05-19 18:35:46 +0530

    install node and set path

--- mkdevenv.sh
+++ mkdevenv.sh
@@ -34,34 +34,54 @@ function kroot_install(){
     virtualenv --python=python3 kroot
     cd kroot/
     source bin/activate
+
+    pip install virtualenvwrapper
+    pip install autopep8 
+    pip install rope
     
+    # to redo 
+    pip freeze | xargs pip uninstall -y 
+
+    # let's use the power of node package manager
+    install_node()
+
+    # Other installs
+    install_vscode()
+    
+    # Let's use the power of pipx
+    #
     # pipx setup (https://pipxproject.github.io/pipx/)
-    pip install pipx  
-    
-    # ! deactivate the kroot venv before running pipx install to avoid " maximum recursion depth exceeded while calling a Python object" error
-    deactivate
-    
+    # pip install pipx 
     # now use pipx to install needed tools for effective dev env
-    mkdir ~/kroot/local/bin
-    mkdir ~/kroot/local/pipx
-    export PIPX_BIN_DIR=~/kroot/local/bin
-    export PIPX_HOME=~/kroot/local/pipx
-    pipx install lolcat # install anything which will trigger shared libraries creation 
-    
-    # monitor the activity
-    watch -d -n 5 "du -ckh /u/krishnam/kroot/local/pipx/"
+    # mkdir ~/kroot/local/bin
+    # mkdir ~/kroot/local/pipx
+    # export PIPX_BIN_DIR=~/kroot/local/bin
+    # export PIPX_HOME=~/kroot/local/pipx
+    # pipx install lolcat # install anything which will trigger shared libraries creation 
+    # watch -d -n 5 "du -ckh /u/krishnam/kroot/local/pipx/"
+    # ERROR: maximum recursion depth exceeded while calling a Python object
 }
 
-function installvscode(){
-    mkdir -p ~/kroot/ide
-    cd ~/kroot/ide
+function install_vscode(){
+    cd ~/kroot
     # download from firefox https://code.visualstudio.com/download
     tar -xvf code-stable-x64-1620838810.tar.gz
     mv VSCode-linux-x64 vscode
     rm code-stable-x64-1620838810.tar.gz
     # make sure this is added to path via rc file
-    PATH="$HOME/kroot/ide/vscode/bin:$PATH"
+    PATH="$HOME/kroot/vscode/bin:$PATH"
 }
+
+function install_node(){
+    cd ~/kroot
+    # download from https://nodejs.org/en/
+    tar -xvf node-v14.17.0-linux-x64.tar.xz
+    mv node-v14.17.0-linux-x64 node
+    rm node-v14.17.0-linux-x64.tar.xz
+    # make sure this is added to path via rc file
+    PATH="$HOME/kroot/node/bin:$PATH"
+}
+
 
 #### What Do I have Already
 function whatdoihave(){
@@ -75,77 +95,72 @@ function whatdoihave(){
 }
 
 function ubuntu_install(){
-# start with venv setup
-sudo apt install python3.9       # python latest
-sudo apt install python3-pip     # package management - Pip Installs Packages
-sudo apt install pipx            # help you install and run end-user applications written in Python into an isolated environment. It's roughly similar to apt-get.
-sudo apt install virtualenv      # provides virtual environment - has its own Python binary and independent set of Python packages
+    # start with venv setup
+    sudo apt install python3.9       # python latest
+    sudo apt install python3-pip     # package management - Pip Installs Packages
+    sudo apt install pipx            # help you install and run end-user applications written in Python into an isolated environment. It's roughly similar to apt-get.
+    sudo apt install virtualenv      # provides virtual environment - has its own Python binary and independent set of Python packages
 
-# workspace
-sudo apt install zsh             # powerful sh 
-sudo apt install tmux            # terminal multiplexer
-sudo apt install nano            # feature-rich CLI text editor for power users
-sudo apt install spell           # used in nano for spell check
-sudo apt install vim             # open-source clone of vi text editor developed to be customizable and able to work with any type of text
+    # workspace
+    sudo apt install zsh             # powerful sh 
+    sudo apt install tmux            # terminal multiplexer
+    sudo apt install nano            # feature-rich CLI text editor for power users
+    sudo apt install spell           # used in nano for spell check
+    sudo apt install vim             # open-source clone of vi text editor developed to be customizable and able to work with any type of text
 
-# coder
-sudo apt install git             # version control
-sudo apt install kdiff3          # compares and merges two or three input files or directories
-sudo apt install code            # VS Code IDE
+    # coder
+    sudo apt install git             # version control
+    sudo apt install kdiff3          # compares and merges two or three input files or directories
+    sudo apt install code            # VS Code IDE
 
-sudo apt install apache2         # web server
-sudo apt install nginx           # web server
-sudo apt install apache2-utils   # ab: apache bench for cli single page load test, htpassword : create pwd
-sudo apt install openssl         # to create ssl certificates
+    sudo apt install apache2         # web server
+    sudo apt install nginx           # web server
+    sudo apt install apache2-utils   # ab: apache bench for cli single page load test, htpassword : create pwd
+    sudo apt install openssl         # to create ssl certificates
 
+    pipx install autopep8            # vscode needs this for auto formatting 
+    pipx install virtualenvwrapper   # provides a set of commands that extend Python virtual environments for more control and better manageability. It places all your virtual environments in one directory
+    pipx install locust              # open source load testing tool, define user behaviour with Python code
+    pipx install rope                # python refactoring library - used in vscode 
 
-pipx install autopep8            # vscode needs this for auto formatting 
-pipx install virtualenvwrapper   # provides a set of commands that extend Python virtual environments for more control and better manageability. It places all your virtual environments in one directory
-pipx install locust              # open source load testing tool, define user behaviour with Python code
-pipx install rope                # python refactoring library - used in vscode 
+    sudo apt install ruby            # need for ruby gems 
+    sudo apt install default-jdk     # open jdk
+    sudo apt install tomcat9         # servlet container
+    sudo apt install nodejs          # servlet container
+    sudo apt install npm             # package manager for the nodejs platform
+    sudo apt install httpie          # user-friendly command-line HTTP client for the API era
 
+    # general tools
+    sudo apt install watch
+    sudo apt install curl            # 
+    sudo apt install tree            # list dir in tree form
+    sudo spt install gawk            # GNU awk
+    sudo apt install ranger          # console file manager with vi key bindings
+    sudo apt install xclip           # clipboard management
+    sudo npm install -g tldr         # TooLongDidntRead: tldr pages are a community effort to simplify the beloved man pages with practical examples
+    sudo apt install rsync           # utility tool for performing swift incremental file transfers 
+    sudo apt install jq              # lightweight and flexible command-line JSON processor. It is like sed for JSON data. use it to slice and filter and map and transform structured data
+    sudo apt install zip             # default in > ubuntu21.04 
+    sudo apt install unzip           # default in > ubuntu21.04 
+    sudo apt install rar             # archive utilities 
+    sudo apt install unrar           # archive utilities 
+    sudo apt install gnome-tweaks    # useful to change capslock key binding
 
-sudo apt install ruby            # need for ruby gems 
-sudo apt install default-jdk     # open jdk
-sudo apt install tomcat9         # servlet container
-sudo apt install nodejs          # servlet container
-sudo apt install npm             # package manager for the nodejs platform
-sudo apt install httpie          # user-friendly command-line HTTP client for the API era
+    # alternatice utilities
+    sudo apt install exa             # more user-friendly version of ls
+    sudo apt install fd-find         # fdfind: a program to find entries in your filesytem. It is a simple, fast and user-friendly alternative to find
+    sudo apt install ncdu            # NCurses Disk Usage: to view and analyse disk space usage. It can drill down into directories and report space used by individual directories. 
+    sudo apt install htop            # interactive process viewer similar to top but that provides a nicer user experience out of the box
+    sudo pip install glances         # system monitoring tool 
+    sudo apt install htop            # top-like interface for container metrics
 
+    sudo apt install sysstat         # iostat - cpu usage
+    sudo apt install lm-sensors      # cpu temp
 
-# general tools
-sudo apt install watch
-sudo apt install curl            # 
-sudo apt install tree            # list dir in tree form
-sudo spt install gawk            # GNU awk
-sudo apt install ranger          # console file manager with vi key bindings
-sudo apt install xclip           # clipboard management
-sudo npm install -g tldr         # TooLongDidntRead: tldr pages are a community effort to simplify the beloved man pages with practical examples
-sudo apt install rsync           # utility tool for performing swift incremental file transfers 
-sudo apt install jq              # lightweight and flexible command-line JSON processor. It is like sed for JSON data. use it to slice and filter and map and transform structured data
-sudo apt install zip             # default in > ubuntu21.04 
-sudo apt install unzip           # default in > ubuntu21.04 
-sudo apt install rar             # archive utilities 
-sudo apt install unrar           # archive utilities 
-sudo apt install gnome-tweaks    # useful to change capslock key binding
-
-# alternatice utilities
-sudo apt install exa             # more user-friendly version of ls
-sudo apt install fd-find         # fdfind: a program to find entries in your filesytem. It is a simple, fast and user-friendly alternative to find
-sudo apt install ncdu            # NCurses Disk Usage: to view and analyse disk space usage. It can drill down into directories and report space used by individual directories. 
-sudo apt install htop            # interactive process viewer similar to top but that provides a nicer user experience out of the box
-sudo pip install glances         # system monitoring tool 
-sudo apt install htop            # top-like interface for container metrics
-
-
-sudo apt install sysstat         # iostat - cpu usage
-sudo apt install lm-sensors      # cpu temp
-
-# just for fun
-sudo apt install cowsay          # An ASCII cow in terminal that will say what ever you want
-sudo apt install figlet          # utility for creating ASCII text banners or large letters out of ordinary text
-sudo apt install cmatrix         # shows a scrolling ‘Matrix‘ like screen in a Linux terminal
-
+    # just for fun
+    sudo apt install cowsay          # An ASCII cow in terminal that will say what ever you want
+    sudo apt install figlet          # utility for creating ASCII text banners or large letters out of ordinary text
+    sudo apt install cmatrix         # shows a scrolling ‘Matrix‘ like screen in a Linux terminal
 }
 
 #####

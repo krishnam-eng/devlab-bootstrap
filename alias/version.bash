8edabd756d3b6196307eb036e6d685e6e6975512
krishnam <krishnam.balamurugan.eng@gmail.com>
2021-05-23 12:34:46 +0530

    global alias to make get version - handy

---
+++ alias/version.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# global alias to get version. e.g. python V => python --version
+alias -g V="--version"
+
+# todo: the tools that does_t support --version are handful, let's handle them differently
+# tmux -V
+# iostat -V
+# try using hist expansion || !:0 -V
+

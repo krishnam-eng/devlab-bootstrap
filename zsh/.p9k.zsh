a7fcbd237920dd9247b67d6e4f204e541991c78f
krishnam-eng <krishnam.balamurugan.eng@gmail.com>
2021-05-25 14:12:29 +0530

    powerline 9 theme

--- zsh/.p9k.zsh
+++ zsh/.p9k.zsh
@@ -1,8 +1,8 @@
 # Customise the Powerlevel9k prompts
 # https://github.com/Powerlevel9k/powerlevel9k#prompt-customization
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-  ssh
-  dir
+ #ssh
+  context
   dir_writable
   vcs
   newline
@@ -13,6 +13,19 @@ POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
   command_execution_time
   background_jobs
   virtualenv
-  history
-  time
+  dir
+ #history
+ #time
 )
+# default is 3
+POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=5
+
+# default 2
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=5
+
+POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
+
+POWERLEVEL9K_MODE=powerline
+
+POWERLEVEL9K_HOST_ICON="\uF109 "
+POWERLEVEL9K_SSH_ICON="\uF489 "

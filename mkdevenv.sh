ba73213d162347fa043849a8e6f351c8a346530f
Balamurugan Krishnamoorthy (Bala) <82016952+krishnam-eng@users.noreply.github.com>
2021-07-14 17:52:22 +0530

    Update mkdevenv.sh

--- mkdevenv.sh
+++ mkdevenv.sh
@@ -224,9 +224,9 @@ function install_in_virtual_env(){
 
     # Other installs
     install_vscode()
-    wget https://services.gradle.org/distributions/gradle-7.0.2-bin.zip
+    wget https://services.gradle.org/distributions/gradle-7.1.1-bin.zip
     unzip gradle-7.0.2-bin.zip
-    export GRADLE_HOME=~/kroot/tools/gradle-7.0.2
+    export GRADLE_HOME=~/kroot/build/gradle-7.1.1
     export PATH=${GRADLE_HOME}/bin:${PATH}
 }
 

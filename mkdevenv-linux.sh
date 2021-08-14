ff8caf0a48317f5d73c98af1e8a0c80a3d14f968
Balamurugan Krishnamoorthy (Bala) <82016952+krishnam-eng@users.noreply.github.com>
2021-08-14 10:02:01 +0530

    refactor and org

--- mkdevenv-linux.sh
+++ mkdevenv-linux.sh
@@ -14,65 +14,14 @@
 #               `create_myconf_links()`
 #############################################################################################
 
-function build_my_workspace_core(){
-
-    # * myws: intend is to make the config root name fixed irrespective of the github repo name. config root name will be refered in many micro automation
-    git clone --depth=1 https://github.com/krishnam-eng/ohmy-linux ~/kroot/myws
-
-    # for home pc: git remote set-url origin git@github.com:krishnam-eng/ohmy-linux
-
-    # create links to tools run configs (or copy and further customize if you want to be disconnected from repo)
-    ln -s ~/kroot/myws/bash/.bashrc  ~/.bashrc
-    ln -s ~/kroot/myws/nano/.nanorc  ~/.nanorc
-    ln -s ~/kroot/myws/tmux/.tmux.conf ~/.tmux.conf
-    ln -s ~/kroot/myws/zsh/.zshenv ~/.zshenv
-    ln -s ~/kroot/myws/git/.gitconfig ~/.gitconfig
-    ln -s ~/kroot/myws/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
-
-    # TMUX plugin manager
-    git clone --depth=1 https://github.com/tmux-plugins/tpm ~/kroot/plugins/tmux/tpm
-    # next: reload tmux conf , and press <prefix> Shift+R to install plugins
-
-    # zsh style - theme & font to boost dev productivity
-    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ~/kroot/style/zsh-syntax-highlighting
-
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/kroot/style/powerlevel10k
-
-    git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git ~/kroot/style/powerlevel9k
-    cp ~/kroot/myws/zsh/powerlevel9k.zsh-theme ~/kroot/style/powerlevel9k/  # fix for old zsh compatability issue
-
-    git clone --depth=1 https://github.com/erikw/tmux-powerline.git  ~/kroot/style/tmux-powerline
-    cp ~/kroot/myws/tmux/tmux-powerline-theme.sh ~/kroot/style/tmux-powerline/themes/default.sh
-
-    # nano editor
-    sudo ln -s ~/kroot/myws/nano/syntax-highlight/yaml.nanorc /usr/share/nano/yaml.nanorc
-
-    # font
-    pip install --user powerline-status
-
-    wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-    mv PowerlineSymbols.otf ~/.local/share/fonts/
-
-    git clone https://github.com/powerline/fonts.git --depth=1  ~/kroot/style/powerline-fonts
-    cd ~/kroot/style/powerline-fonts
-    ./install.sh
-
-    fc-cache -vf ~/.local/share/fonts/
-
-    wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-    mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
-
-    fc-list
-    # check https://powerline.readthedocs.io/en/latest/installation/linux.html#fonts-installation
-}
-
 function build_file_hierarchy_structure(){
     # create _k_rishnam config _root_
 
     #l1
     mkdir ~/kroot
     mkdir ~/proj
-    mkdir ~/bkp
+    mkdir ~/bkp        # long live
+    mkdir ~/.mybkp     # temp 
     mkdir ~/log
     mkdir ~/tmp
 
@@ -100,51 +49,134 @@ function build_file_hierarchy_structure(){
 
     #l4
     mkdir ~/kroot/plugins/tmux/tpm
+
+
+    
+    # Check
+    sudo apt install tree
+    tree
+    
+}
+
+function build_my_workspace_core(){
+
+    # core install (might be already installed)
+    sudo apt install git 
+    sudo apt install nano            # feature-rich CLI text editor for power users 
+    sudo apt install tmux            # terminal multiplexer
+    sudo apt install zsh             # powerful sh
+    
+    # * myws: intend is to make the config root name fixed irrespective of the github repo name. config root name will be refered in many micro automation
+    git clone --depth=1 https://github.com/krishnam-eng/ohmy-linux ~/kroot/myws
+
+    # for home pc: git remote set-url origin git@github.com:krishnam-eng/ohmy-linux
+
+    # create links to tools run configs (or copy and further customize if you want to be disconnected from repo)
+    cp ~/.bashrc ~/.mybkp/.bashrc_$(date +%y%m%d)-old
+    cp ~/.nanorc ~/.mybkp/.nanorc_$(date +%y%m%d)-old
+    cp ~/.tmux.conf ~/.mybkp/.tmux.conf_$(date +%y%m%d)-old
+    cp ~/.zshenv ~/.mybkp/.zshenv_$(date +%y%m%d)-old
+    cp ~/.zshrc ~/.mybkp/.zshrc_$(date +%y%m%d)-old
+
+    rm -f  ~/.zshenv ~/.zshrc ~/.bashrc ~/.tmux.conf ~/.nanorc
+    
+    ln -s ~/kroot/myws/bash/.bashrc  ~/.bashrc
+    ln -s ~/kroot/myws/nano/.nanorc  ~/.nanorc
+    ln -s ~/kroot/myws/tmux/.tmux.conf ~/.tmux.conf
+    ln -s ~/kroot/myws/zsh/.zshenv ~/.zshenv
+    ln -s ~/kroot/myws/git/.gitconfig ~/.gitconfig
+
+    # TMUX plugin manager
+    git clone --depth=1 https://github.com/tmux-plugins/tpm ~/kroot/plugins/tmux/tpm
+    # next: reload tmux conf , and press <prefix> Shift+R to install plugins
+
+    # zsh style - theme & font to boost dev productivity
+    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ~/kroot/style/zsh-syntax-highlighting
+
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/kroot/style/powerlevel10k
+
+    #git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git ~/kroot/style/powerlevel9k
+    #cp ~/kroot/myws/zsh/powerlevel9k.zsh-theme ~/kroot/style/powerlevel9k/  # fix for old zsh compatability issue
+
+    git clone --depth=1 https://github.com/erikw/tmux-powerline.git  ~/kroot/style/tmux-powerline
+    cp ~/kroot/myws/tmux/tmux-powerline-theme.sh ~/kroot/style/tmux-powerline/themes/default.sh
+
+    # nano editor
+    sudo ln -s ~/kroot/myws/nano/syntax-highlight/yaml.nanorc /usr/share/nano/yaml.nanorc
+}
+
+function install_fonts_unstable_do_it_last_after_snapshot(){
+    # font
+    pip install --user powerline-status
+
+    wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+    mv PowerlineSymbols.otf ~/.local/share/fonts/
+
+    git clone https://github.com/powerline/fonts.git --depth=1  ~/kroot/style/powerline-fonts
+    cd ~/kroot/style/powerline-fonts
+    ./install.sh
+
+    fc-cache -vf ~/.local/share/fonts/
+
+    wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+    mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+    fc-list
+    # check https://powerline.readthedocs.io/en/latest/installation/linux.html#fonts-installation
 }
 
 function install_in_ubuntu_env(){
     # to get the latest version, it is preferable to use ppa - personal package archive repo over default ubuntu repo
+    # Install Optional items later when needed
 
-
-    # start with venv setup
-    sudo apt install python3.9       # python latest
-    sudo apt install python3-pip     # package management - Pip Installs Packages
-    sudo apt install pipx            # help you install and run end-user applications written in Python into an isolated environment. It's roughly similar to apt-get.
-    sudo apt install virtualenv      # provides virtual environment - has its own Python binary and independent set of Python packages
-
-    # workspace
-    sudo apt install zsh             # powerful sh
-    sudo apt install fish            # firendly interactive shell - using it for default shell in guack
-    sudo apt install tmux            # terminal multiplexer
-    sudo apt install gnome-tweaks    # useful to change capslock key binding
+    # good system utils & dev tools to start with
     sudo apt install guake           # drop down terminal emulator - # ! use x (x11 or xarg) display server since keybinding doesn't work well in wyaland server
-    sudo ln -s /usr/share/applications/guake.desktop /etc/xdg/autostart/ # add it to auto start
-    sudo apt install nano            # feature-rich CLI text editor for power users
+    sudo ln -s /usr/share/applications/guake.desktop /etc/xdg/autostart/
     sudo apt install vim             # open-source clone of vi text editor developed to be customizable and able to work with any type of text
-
-    # coder
-    sudo apt install git             # version control
-    sudo apt install kdiff3          # compares and merges two or three input files or directories
-    sudo apt install code            # IDE for light weight project (python, scripts)
-    sudo snap install intellij-idea-community --classic # IDE for JVM
-
-    pip install autopep8            # vscode needs this for auto formatting
-    pip install virtualenvwrapper   # provides a set of commands that extend Python virtual environments for more control and better manageability. It places all your virtual environments in one directory
-    pip install locust              # open source load testing tool, define user behaviour with Python code
-    pip install rope                # python refactoring library - used in vscode
-
-
-    sudo apt install docker-ce
-    sudo apt install docker-ce-cli
-    sudo apt install containerd.io
-
-    # this will give latest version
+    sudo apt install watch           # 
+    sudo apt install curl            #
+    sudo apt install tree            # list dir in tree form
+    sudo apt install gawk            # GNU awk  
+    sudo apt install httpie          # user-friendly command-line HTTP client for the API era
+    
+    
+    # start with venv & pydev setup
+    sudo apt install python3.9       # python latest (already installed)
+    sudo apt install python3-pip     # package management - Pip Installs Packages
+    pip install autopep8             # vscode needs this for auto formatting
+    sudo apt install virtualenv      # provides virtual environment - has its own Python binary and independent set of Python packages
+    pip install virtualenvwrapper    # provides a set of commands that extend Python virtual environments for more control and better manageability. It places all your virtual environments in one directory
+    sudo snap install --classic code # IDE for light weight project (python, scripts)
+    sudo apt install pipx            # [optional] help you install and run end-user applications written in Python into an isolated environment. It's roughly similar to apt-get.
+    pip install locust               # [optional] open source load testing tool, define user behaviour with Python code
+    pip install rope                 # [optional] python refactoring library - used in vscode
+    sudo snap install intellij-idea-community --classic # [optional] IDE for JVM
+    sudo apt install kdiff3          # [optional] compares and merges two or three input files or directories      
+    sudo apt install fish            # [optional] firendly interactive shell - using it for default shell in guack
+    ln -s ~/kroot/myws/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+    sudo apt install gnome-tweaks    # [optional] useful to change capslock key binding
+    
+    # Build Tools
+    sudo add-apt-repository ppa:cwchien/gradle # use this repo to get latest gradle version
+    sudo apt install gradle
+    
+    # Container
+        # Check latest instructions from - https://docs.docker.com/engine/install/ubuntu/
+        # Step 1: Set up the repository
+    sudo apt-get update
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-releasesudo apt install docker-ce
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        # Step 2: Install Docker Engine
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+        # Step3: Check
+    sudo docker run hello-world
+    # [optional] this will give latest version
     VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r
     DESTINATION=/usr/local/bin/docker-compose
     sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
     sudo chmod 755 $DESTINATION
-
-    sudo apt install bridge-utils    # brctr - ethernet brdige admin cmd
 
     # on-demand
     sudo apt install default-jdk     # open jdk
@@ -158,15 +190,11 @@ function install_in_ubuntu_env(){
     sudo apt install openssl         # to create ssl certificates
     sudo apt install ruby            # need for ruby gems
 
-    # use this repo to get latest gradle version
-    sudo add-apt-repository ppa:cwchien/gradle
-    sudo apt install gradle
+
 
     # general tools
-    sudo apt install watch
-    sudo apt install curl            #
-    sudo apt install tree            # list dir in tree form
-    sudo spt install gawk            # GNU awk
+    sudo apt install bridge-utils    # brctr - ethernet brdige admin cmd
+
     # sudo apt install ranger        # console file manager with vi key bindings (npm error: Not compatible with your version of node/npm)
     sudo apt install xclip           # clipboard management
     sudo apt install rsync           # utility tool for performing swift incremental file transfers
@@ -310,16 +338,7 @@ function whatdoihave(){
 
 
 function backup_old_conf(){
-    # take a backup of old configs
-    mkdir -p ~/.mybkp
 
-    cp ~/.bashrc ~/.mybkp/.bashrc_$(date +%y%m%d)-old
-    cp ~/.nanorc ~/.mybkp/.nanorc_$(date +%y%m%d)-old
-    cp ~/.tmux.conf ~/.mybkp/.tmux.conf_$(date +%y%m%d)-old
-    cp ~/.zshenv ~/.mybkp/.zshenv_$(date +%y%m%d)-old
-    cp ~/.zshrc ~/.mybkp/.zshrc_$(date +%y%m%d)-old
-
-    rm -f  ~/.zshenv ~/.zshrc ~/.bashrc ~/.tmux.conf ~/.nanorc
 
 }
 

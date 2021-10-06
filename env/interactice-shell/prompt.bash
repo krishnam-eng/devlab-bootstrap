9d3476b3a9abfd29464d4c1598aa5303e4bb074e
Balamurugan Krishnamoorthy (Bala) <82016952+krishnam-eng@users.noreply.github.com>
2021-10-06 13:22:43 +0530

    Update prompt.bash

--- env/interactice-shell/prompt.bash
+++ env/interactice-shell/prompt.bash
@@ -5,7 +5,8 @@ get_branchname(){
   git branch 2>/dev/null | awk '/\*/{print $2}'
 }
 
+shorthost=$(hostname | sed -e 's/BA.*//')
 
-export PROMPT="%F{154}%n%f@%F{011}%m%F{010}%#%f "
+export PROMPT="%F{154}%n%f@%F{011}${shorthost}%F{010}%#%f "
 
-export RPROMPT=" %F{010}[%F{011}%~ %F{154}%*%F{010}]"
+export RPROMPT="%(?.%F{green}%h √.%F{red}%h) %F{010}[%F{011}%~ %F{154}%*%F{010}]"

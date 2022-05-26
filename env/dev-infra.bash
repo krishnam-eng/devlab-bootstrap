cc9f262b2ec5de8fc238b117ee74d2ab9481094e
bala <krishnam.balamurugan.eng@gmail.com>
2022-05-26 10:54:30 +0530

    master vault & kubectl

--- env/dev-infra.bash
+++ env/dev-infra.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-
+#####
+#   Hashicorp Vault
+#####
 # This will help vault client to talk to local vault server
 export VAULT_ADDR='http://127.0.0.1:8200'
+
+# export VAULT_TOKEN="$(vault token create -field token)"

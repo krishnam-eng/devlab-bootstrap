5a1a77a5f9680a304d13f61c94e32196c5cc14c3
bala <krishnam.balamurugan.eng@gmail.com>
2022-07-16 12:20:20 +0550

    [Auto committed] lib updates

--- lib/main.py
+++ lib/main.py
@@ -2,10 +2,7 @@ import subprocess
 import git
 import time
 from git import GitCommandError
-from mdutils.mdutils import MdUtils
 
-
-# todo: avoid empty commit
 
 def backup_safari_bookmarks(source_file='~/Library/Safari/Bookmarks.plist',
                             destination_folder='~/hrt/vault/bookmarks/safari/'):
@@ -14,16 +11,16 @@ def backup_safari_bookmarks(source_file='~/Library/Safari/Bookmarks.plist',
     print("\t >  {}".format(completed_process.stdout))
 
 
-def backup_devws(repo_path='~/hrt/vault/'):
+def sync_homelab(repo_path='~/hrt/boot/'):
     try:
         repo = git.Repo(repo_path)
 
         if is_any_changes(repo):
             print_git_status(repo)
 
-            source_dirs = ["postman", "scripts", "intellij", "bookmarks", "dbeaver", "springboot-profiles", "."]
+            source_dirs = ["lib", "custom", "shell", "tools", "os", "."]
             for sd in source_dirs:
-                git_add_commit(repo, sd)
+                stage_and_commit(repo, sd)
 
             origin = repo.remote(name='origin')
             origin.push()
@@ -37,12 +34,19 @@ def backup_devws(repo_path='~/hrt/vault/'):
         raise
 
 
+def update_packages():
+    brew_cmds = ['update', 'outdated', 'upgrade', 'cleanup']
+    for bc in brew_cmds:
+        print_h2('Brew {}'.format(bc))
+        x = subprocess.run("brew {}".format(bc), shell=True)
+
+
 def is_any_changes(repo):
     status = repo.git.status('--short')
     return status != ''
 
 
-def git_add_commit(repo, source_dir):
+def stage_and_commit(repo, source_dir):
     repo.git.add([source_dir])
     if out_of_sync(repo, source_dir):
         commit_obj = repo.index.commit('[Auto committed] {} updates'.format(source_dir))
@@ -90,45 +94,22 @@ def print_footnote(note):
     print('{} \n'.format(note))
 
 
-def update_local_luminos_repos():
-    repo_root = "~/proj/luminos/luminos-rewards-"
-    repo_names = ["apms", "commons", "configuration", "event-processing", "issuance", "orchestration"]
-    for rn in repo_names:
-        repo = git.Repo(repo_root + rn)
-        try:
-            pstatus = repo.git.pull()
-            print_around('{}: Update Success'.format(rn), pstatus)
-        except GitCommandError as e:
-            print_around('{}: Update Failed'.format(rn), e)
-
-
-def update_packages():
-    brew_cmds = ['update', 'outdated', 'upgrade', 'cleanup']
-    for bc in brew_cmds:
-        print_h2('Brew {}'.format(bc))
-        x = subprocess.run("brew {}".format(bc), shell=True)
-        print(x)
-
-
 if __name__ == '__main__':
     counter = time.perf_counter()
 
-    print_h1("Backup Safari Bookmarks [safari -> ws-settings]...")
+    print_h1("Vault: Backup Safari Bookmarks...")
     backup_safari_bookmarks()
 
-    print_h1("Sync Dev Workspace Settings [local -> remote]...")    q
-    backup_devws()
-
-    print_h1("Update Luminos Repositories [remote -> local]...")
-    update_local_luminos_repos()
-
-    print_h1("Update Taps (third-party repositories) and Bottles (binary packages)..."
+    print_h1("HomeBrew: Update Taps (third-party repositories) and Bottles (binary packages)..."
              "[remote -> local]")
     update_packages()
 
+    print_h1("GitHub: Sync Dev Workspace - HomeLab, Tools Settings [local -> remote]...")
+    sync_homelab()
+
     print_footnote("My brain hurts! Let's automate more !")
+
     print('But, Now !... [at least weekly once]')
-    print('Export Postman Collections & Envs => [... -> Export]')
     print('Export DBeaver Preferences        => [File -> Export]')
     print('Export IDE IDEA Preferences       => [File -> Manage IDE Settings -> Export]')
     print('Export IDE VSCode Preferences     => [Code -> Preferences -> Settings Sync is On]')

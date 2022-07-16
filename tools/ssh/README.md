d5d0f0fcf91020eb49faf99731d87f4dbdb810c6
bala <krishnam.balamurugan.eng@gmail.com>
2022-07-16 11:09:20 +0530

    simplyfy root dirs

---
+++ tools/ssh/README.md
@@ -0,0 +1,8 @@
+```Sorry, no pub & private key here :) Just basic config & learing notes ```
+
+
+Useful Quick Recap:
+
+- Forward & Reverse Tunning: https://www.youtube.com/watch?v=AtuAdk4MwWw&ab_channel=Tinkernut
+
+
---
+++ tools/venv/virtualenv.sh
@@ -0,0 +1,24 @@
+# virtual environment keeps dependencies required by different projects in separate places.
+
+# install venv
+sudo apt-get install python-virtualenv
+# or
+pip3 install --user virtualenv
+
+# You can specify the version of Python with the --python argument.
+# Then, use the activate script to set the PATH, entering the virtual environment:
+
+cd my-project-folder
+virtualenv --python python3 my-venv
+source my-venv/bin/activate
+
+# When bundling your own packages or projects for other people, you can use:
+pip freeze > requirements.txt
+
+# Collaborators can install all of the dependencies in their own virtual environment when given a requirements.txt file by typing:
+pip install -r requirements.txt
+
+# To return to normal system settings, type
+deactivate
+
+#* created alias for all venv commands - check alias/venv.bash

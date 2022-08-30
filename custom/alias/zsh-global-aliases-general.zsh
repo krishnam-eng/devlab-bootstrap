59f3f8fb3803bc385c6113fde9a897894f5a1b82
bala <krishnam.balamurugan.eng@gmail.com>
2022-08-30 19:54:42 +0530

    alias for jq & auto complete for terraform

--- custom/alias/zsh-global-aliases-general.zsh
+++ custom/alias/zsh-global-aliases-general.zsh
@@ -9,6 +9,7 @@ alias -g H='| head'
 alias -g T='| tail'
 alias -g M='| more'
 alias -g L='| less'
+alias -g J='| jq .'    # json pretty print - colorful output
 alias -g R='| lolcat'
 alias -g G='| egrep'
 alias -g GW='| egrep -w' # grep for a word (not partial match)

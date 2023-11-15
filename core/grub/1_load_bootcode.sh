4799a5ebae8043a7f4171b1862d5453fac0f55da
bala <krishnam.balamurugan.eng@gmail.com>
2023-11-15 19:22:33 +0530

    bootloader impl - zsh, list

--- core/grub/1_load_bootcode.sh
+++ core/grub/1_load_bootcode.sh
@@ -1,27 +1,52 @@
 #!/usr/bin/env bash
-################
-# description  : Boot Loader for devbox -  a convention based utility to make the dev related micro automations and configuration for productivity boost.
-# author	        : Bala K
-################
 
 # Load HomeLab source code into hrt to configure DevBox
-function load_boot_source(){
+function main(){
   _install_dependencies
 
-  # Homelab RooT (HRT). ~/hrt will be the heart of the devbox. It contains all the files needed for successful booting process.
-  mkdir $USER/hrt
-  cd $USER/hrt
+  _checkout_bootcode
 
-  # copy and add key to github ssh keys to access private repo
-  ssh-keygen
-  cat ~/.ssh/github.pub
-  git clone --depth=1 git@github.com:krishnam-eng/homelab-devbox.git ~/hrt/boot
+  _make_zsh_default_shell
+  _load_aliases
+}
+
+function _checkout_bootcode() {
+    # Homelab RooT (HRT). ~/hrt will be the heart of the devbox. It contains all the files needed for successful booting process.
+    mkdir $USER/hrt
+    cd $USER/hrt
+
+    # ! Manual Step: copy and add key to github ssh keys to access private repo
+    ssh-keygen
+    cat ~/.ssh/github.pub
+    git clone --depth=1 git@github.com:krishnam-eng/homelab-devbox.git $HOME/hrt/boot
+    tree -L 3 ~/hrt/boot
+}
+
+
+function _make_zsh_default_shell() {
+    # current default shell
+    echo $SHELL
+
+    # list available shells
+    cat /etc/shells
+
+    # change default shell to zsh
+    zsh --version
+    sudo chsh -s $(which zsh)
+
+  # ! Log out and log back in to see the change
+}
+
+# load named directories & list aliases
+function _load_aliases() {
+      source $HOME/hrt/boot/custom/alias/boot-list.zsh
+      source $HOME/hrt/boot/custom/alias/boot-nameddirs.zsh
 }
 
 function _install_dependencies(){
+    sudo apt install zsh
+
     sudo apt install git
+
     sudo apt install tree
 }
-
-
-
---
+++ custom/alias/boot-nameddirs.zsh
@@ -0,0 +1,23 @@
+##################
+# Named Directory
+#
+#   ~shortcutname: Creating Named Dir by adding  entry in "fil/dir names hash table."
+#
+#   Named dir can be called like ~shortcutname  similar to how ~ refers to home dir
+#
+###################
+
+hash -d hrt=~/hrt
+
+hash -d boot=~/hrt/boot
+
+hash -d zsh=~/hrt/boot/conf/zsh
+
+hash -d alias=~/hrt/boot/custom/alias
+hash -d env=~/hrt/boot/custom/env
+hash -d func=~/hrt/boot/custom/func
+
+hash -d ssh=~/hrt/boot/settings/ssh
+
+hash -d vim=~/hrt/boot/settings/vim
+hash -d nano=~/hrt/boot/settings/nano

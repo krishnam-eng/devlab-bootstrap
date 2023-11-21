817f1a366f4db68107b1b3b46ea3d80d0b39c464
bala <krishnam.balamurugan.eng@gmail.com>
2023-11-21 09:59:57 +0530

    restruct;assume yes install

--- admin/init/4_dev_tools_highlevel.sh
+++ admin/init/4_dev_tools_highlevel.sh
@@ -1,26 +1,26 @@
 function load_dev_tools(){
     # Java Dev: Basic Development Tools & Others
-    sudo apt install openjdk-11-jre-headless
-    sudo apt install openjdk-8-jre-headless
+    sudo apt install -y openjdk-11-jre-headless
+    sudo apt install -y openjdk-8-jre-headless
     sudo add-apt-repository ppa:cwchien/gradle # use this repo to get latest gradle version
-    sudo apt install gradle          # build tool & dependency manager
-    sudo apt install httpie          # user-friendly command-line HTTP client for the API era
-    sudo apt install apache2         # [optional] web server
-    sudo apt install nginx           # [optional] web server
-    sudo apt install tomcat9         # [optional] servlet container
-    sudo apt install nodejs          # [optional] servlet container
-    sudo apt install npm             # [optional] package manager for the nodejs platform
-    sudo apt install apache2-utils   # [optional] ab: apache bench for cli single page load test, htpassword : create pwd
-    sudo apt install openssl         # [optional] to create ssl certificates
+    sudo apt install -y gradle          # build tool & dependency manager
+    sudo apt install -y httpie          # user-friendly command-line HTTP client for the API era
+    sudo apt install -y apache2         # [optional] web server
+    sudo apt install -y nginx           # [optional] web server
+    sudo apt install -y tomcat9         # [optional] servlet container
+    sudo apt install -y nodejs          # [optional] servlet container
+    sudo apt install -y npm             # [optional] package manager for the nodejs platform
+    sudo apt install -y apache2-utils   # [optional] ab: apache bench for cli single page load test, htpassword : create pwd
+    sudo apt install -y openssl         # [optional] to create ssl certificates
     sudo snap install intellij-idea-community --classic # [optional] IDE for JVM
 
     # Python Dev: Virtual Env & Others
-    sudo apt install python3.9       # python latest (already installed)
-    sudo apt install python3-pip     # package management - Pip Installs Packages
+    sudo apt install -y python3.9       # python latest (already installed)
+    sudo apt install -y python3-pip     # package management - Pip Installs Packages
     pip install autopep8             # vscode needs this for auto formatting
-    sudo apt install virtualenv      # provides virtual environment - has its own Python binary and independent set of Python packages
+    sudo apt install -y virtualenv      # provides virtual environment - has its own Python binary and independent set of Python packages
     pip install virtualenvwrapper    # provides a set of commands that extend Python virtual environments for more control and better manageability. It places all your virtual environments in one directory
-    sudo apt install pipx            # [optional] help you install and run end-user applications written in Python into an isolated environment. It's roughly similar to apt-get.
+    sudo apt install -y pipx            # [optional] help you install and run end-user applications written in Python into an isolated environment. It's roughly similar to apt-get.
     pip install locust               # [optional] open source load testing tool, define user behaviour with Python code
     pip install rope                 # [optional] python refactoring library - used in vscode
     sudo snap install --classic code # IDE for light weight project (python, scripts)
@@ -29,7 +29,7 @@ function load_dev_tools(){
     # https://docs.docker.com/engine/install/ubuntu/
     # Step 1: Set up the repository
     sudo apt-get update
-    sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-releasesudo apt install docker-ce
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-releasesudo apt install -y docker-ce
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     # Step 2: Install Docker Engine
@@ -44,10 +44,10 @@ function load_dev_tools(){
     docker-compose --version
 
     # just for fun
-    sudo apt install cowsay            # An ASCII cow in terminal that will say what ever you want
-    sudo apt install figlet            # utility for creating ASCII text banners or large letters out of ordinary text
-    sudo apt install cmatrix           # shows a scrolling ‘Matrix‘ like screen in a Linux terminal [Not in venv setup]
-    sudo apt install lolcat            # [Not in venv setup]
+    sudo apt install -y cowsay            # An ASCII cow in terminal that will say what ever you want
+    sudo apt install -y figlet            # utility for creating ASCII text banners or large letters out of ordinary text
+    sudo apt install -y cmatrix           # shows a scrolling ‘Matrix‘ like screen in a Linux terminal [Not in venv setup]
+    sudo apt install -y lolcat            # [Not in venv setup]
 
     # clean if any package is no longer needed
     sudo apt update; sudo apt upgrade ; sudo apt autoremove
